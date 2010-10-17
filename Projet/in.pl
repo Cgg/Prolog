76517e3f7d56557e85ad37a4858745a686e9b4db
Cgg <clement.geiger@gmail.com>
2010-10-17 14:45:10 +0200

    *ajout d'une contrainte disj

--- Projet/in.pl
+++ Projet/in.pl
@@ -1,6 +1,6 @@
 /*
-* Ceci est un fichier Prolog, espèce de vim de mes klzhenf!
-*/
+ * Ceci est un fichier Prolog, espèce de vim de mes klzhenf!
+ */
 
 it(a,30). %% pour a est un intervalle de durée 30
 it(z,5). %% si la duréé est inconnue on mettrait
@@ -11,18 +11,5 @@ it(c,6).
 
 ctrt([avant,a,z]).
 ctrt([avant,z,b]).
-ctrt([avant,b,c]).
 
-%% Comme cela ne vous aura pas échappé il s’agit de l’exemple sur la
-%% méthode pert traité en td 3if. Il est réalisable en 93 unités de temps.
-%% On pourrait ajouter aussi la contrainte de disjonction entre les tâches
-%% a,b,c,e,f,h (toujours comme dans le td)
-
-% ctrt([allDisj,a,b,c,e,f,h]).
-
-%% ou plus artisanalement :
-
-% ctrt([disj,a,b]). ctrt([disj,a,c]). ctrt([disj,a,e]). ctrt([disj,f,h]).
-
-%% Avec ces disjonctions il faut 98 unités de temps pour réaliser l'ensemble.
-
+ctrt([disj,a,c]).
