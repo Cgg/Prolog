d270667ba6cfd53c671a035add0f7651a98deef8
Cgg <clement.geiger@gmail.com>
2010-10-17 15:46:47 +0200

    Le projet il est fini.

--- Projet/intervalles.pl
+++ Projet/intervalles.pl
@@ -9,28 +9,30 @@
 :- use_module( library( 'clp/bounds' ) ).
 
 /* procedure de remplissage de la liste des intervalles */
-fill(L,LR) :- it( X, T ),
-             not( member( [ X,_,_,_ ], L ) ), !,
-             append( L, [ [ X, _, _, T ] ], L1 ),
-	     fill( L1, LR ).
-fill(L,L).
+fill( L, LR, TMax ) :-
+     it( X, T ),
+     not( member( [ X,_,_,_ ], L ) ), !,
+     append( L, [ [ X, _, _, T ] ], L1 ),
+     add_contrainte( X, L1, TMax ),
+     fill( L1, LR, TMax ).
+fill( L, L, TMax ).
 
 /* procedure de remplissage des contraintes */
 add_contrainte( I, LIT, TMax ) :-
 	member( [ I, D_I, F_I, Duree_I ], LIT ),
-	D_I #> -1,
-	F_I #< TMax + 1,
+	D_I #>= 0,
+	F_I #=< TMax ,
 	( F_I - D_I ) #= Duree_I.
 	
 add_contrainte( avant, I1, I2, LIT ) :-
 	member( [ I1,_,F1,_ ], LIT ),
 	member( [ I2,D2,_,_ ], LIT ),
-	F1 #< D2 + 1.
+	F1 #=< D2.
 
 add_contrainte( disj, I1, I2, LIT ) :-
-	member( [ I1,_,F1,_ ], LIT ),
-	member( [ I2,D2,_,_ ], LIT ),
-	F1 #< D2 #\/ D1 #> F2.
+	member( [ I1,D1,F1,_ ], LIT ),
+	member( [ I2,D2,F2,_ ], LIT ),
+	F1 #=< D2 #\/ D1 #>= F2 .
 
 fill_contrainte( LC, LRC, LI, TMax ) :-
 	ctrt( X ),
@@ -39,10 +41,8 @@ fill_contrainte( LC, LRC, LI, TMax ) :-
 	nth0( 0, X, T ),
 	nth0( 1, X, I ),
 	nth0( 2, X, J ),
-	add_contrainte( I, LIT, TMax ),
-	add_contrainte( J, LIT, TMax ),
-	add_contrainte( T, I, J, LIT ),
-	fill_contrainte( L1, LRC, LIT, TMax ).
+	add_contrainte( T, I, J, LI ),
+	fill_contrainte( L1, LRC, LI, TMax ).
 fill_contrainte( LC, LC, LIT, TMax ).
 
 /* Fonction principale */
@@ -54,14 +54,14 @@ consult( Input ),
 /* Intervalles */
 LI = [],
 
-fill( LI, R_LI ),
+fill( LI, R_LI, TMax ),
 
 /* Contraintes */
 LC = [],
 
 fill_contrainte( LC, R_LC, R_LI, TMax ),
 
-/* La liste est "applatie" avec flatten pour un affichage du resultat avec label */
+/* La liste est "aplatie" avec flatten pour un affichage du resultat avec label */
 flatten( R_LI, R_LI_F ),
 
 label( R_LI_F )
